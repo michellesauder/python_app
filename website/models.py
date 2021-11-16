5eb99181b12fb359d353eda6216346623b2f465f
Michelle Sauder <mgsauder@hotmail.com>
2021-11-16 13:41:12 -0800

    added listing model

--- website/models.py
+++ website/models.py
@@ -16,8 +16,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    listings = db.relationship('Listing')
 
-class Listings(db.Model):
+class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(1000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())

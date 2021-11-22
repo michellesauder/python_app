8a12b2119aa094dab3ac28e2bf6b8436946a29ba
Michelle Sauder <mgsauder@hotmail.com>
2021-11-22 13:04:54 -0800

    issues with db

--- website/__init__.py
+++ website/__init__.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-DB_NAME = "database1.db"
+migrate = Migrate()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    migrate.init_app(app, db)
     db.init_app(app)
+
 
 
     from .views import views
